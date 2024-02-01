df5ebf5d8809a2009eb5bfed7bd47f68562530b5
Wahtu Astrawan <chatt699@gmail.com>
2024-02-01 10:47:40 +0800

    adding handler to file uploading and get the file metadata info

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage: storage});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +23,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+  const fileMetadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
 
+  res.json(fileMetadata);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
